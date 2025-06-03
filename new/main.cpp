76e426515d0262b17c9a3382cab66270a30dff25
err0rgod <ndgaming458@gmail.com>
2025-06-03 14:02:15 +0530

    completed fx to fx inbtegration

--- new/main.cpp
+++ new/main.cpp
@@ -1,10 +1,16 @@
-include <iostream>
+#include <iostream>
 using namespace std;
+
+void Inc(int &x) {
+    x++;
+}
 
 int main() {
     int a = 5;
+    Inc(a);
 
-    
+    cout << "Value of a after increment: " << a << endl;
+
     return 0;
 }
 
