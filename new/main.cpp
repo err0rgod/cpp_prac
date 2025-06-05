dc7bcb4efc06d0b402f440503feb3e817df77025
err0rgod <ndgaming458@gmail.com>
2025-06-05 13:49:12 +0530

    .

--- new/main.cpp
+++ new/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void Inc(int &x) {
-    x++;
+int sumall(int a[], int size){
+    int i, sum = 0;
+    for(i = 0;i<size;i++){
+        sum+= a[i];
+
+    }
+    return sum;
 }
 
-int main() {
-    int a = 5;
-    Inc(a);
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    int size = sizeof(a)/sizeof(a[0]);
+    int total = sumall(a,size);
 
-    cout << "Value of a after increment: " << a << endl;
-
-    return 0;
+    printf("%d is the sum of all elements in array",total);
 }
-
-
--- new/main1.c
+++ new/main1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int sumall(int a[], int size){
+    int i, sum = 0;
+    for(i = 0;i<size;i++){
+        sum+= a[i];
+
+    }
+    return sum;
+}
+
 int main()
 {
     int a[] = {1, 2, 3, 4, 5};
-    printf("%d", *a);
+    int size = sizeof(a)/sizeof(a[0]);
+    int total = sumall(a,size);
+
+    printf("%d is the sum of all elements in array",total);
 }
