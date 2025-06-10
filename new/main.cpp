03840b9b7815d8760ce1cbb150d3003d93e198d9
err0rgod <ndgaming458@gmail.com>
2025-06-10 20:20:55 +0530

    wallah wallah

--- new/main.cpp
+++ new/main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 using namespace std;
+#include <string>
 
-int sumall(int a[], int size){
-    int i, sum = 0;
-    for(i = 0;i<size;i++){
-        sum+= a[i];
 
-    }
-    return sum;
-}
+int main(){
+    string m = "err0r god";
 
-int main()
-{
-    int a[] = {1, 2, 3, 4, 5};
-    int size = sizeof(a)/sizeof(a[0]);
-    int total = sumall(a,size);
+    char c[] = "err0r god";
 
-    printf("%d is the sum of all elements in array",total);
+    char *c1 = c;
+    c1[1] = 'E'; // Changing the first character of the character array
+
+    cout << "String: " << m << endl;
+    cout << "Character Array: " << c1 << endl;
 }
