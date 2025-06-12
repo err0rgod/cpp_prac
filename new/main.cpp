b4c5cb613c794e7db177764b3c16676f3b5466c2
err0rgod <ndgaming458@gmail.com>
2025-06-13 02:09:29 +0530

    cccc

--- new/main.cpp
+++ new/main.cpp
@@ -3,7 +3,9 @@ using namespace std;
 #include <string>
 
 
-
+void print(){
+    cout << "Hello, World!" << endl;
+}
 
 int add(int a, int b) {
     int c = a + b;
@@ -14,6 +16,8 @@ int main(){
     int a =2;
     int b = 3;
 
+
     int c = add(a,b);
+    print();
     cout << "The sum of " << a << " and " << b << " is: " << c << endl;
 }
