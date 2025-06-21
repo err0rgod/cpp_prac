44f88663981d7dfe560c0eb52c90430c513c99f0
err0rgod <ndgaming458@gmail.com>
2025-06-22 01:14:41 +0530

    yum yum

---
+++ dashb.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct studentdata{
+    char name[];
+    int roll[];
+    float sub1[];
+    float sub2[];
+    float sub3[];
+
+}
+
+int main(){
+    struct studentdata s1;
+
+    cout<<"Enter Your Option" << endl;
+    cout << "Option 1 fetch all students name" << endl;
+    
+    char name1 = 'error';
+
+    s1.name[0] = name1;
+    s1.roll[0] = 1;
+    s1.sub1[0] = 12.2;
+
+
+
+   
+    
+}
