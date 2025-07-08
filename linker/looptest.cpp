dc69547b4cd786e8df275428aa3ffd16548389b2
err0rgod <ndgaming458@gmail.com>
2025-07-08 17:34:26 +0530

    got it

--- linker/looptest.cpp
+++ linker/looptest.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void test(){
+    cout<<"test"<<endl;
+    main();
+    
+}
+
 int main(){
 
     cout<<"main"<<endl;
     test();
+
+    return 0;
 }
 
 
-void test(){
-    cout<<"test"<<endl;
-    main();
-}
