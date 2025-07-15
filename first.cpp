ab0cab7fd7fe96acbcf43958528df1d134ecf451
err0rgod <ndgaming458@gmail.com>
2025-07-15 22:27:10 +0530

    d

--- first.cpp
+++ first.cpp
@@ -10,12 +10,13 @@ int main(){
     cin >> x;
     cin >> y;
 
-    auto f = [](int a, int b){
-        std::cout << "lAMBda function working" << std::endl;
+    auto f = [](int a, int b)-> int{
+       
         cout << a/b << endl;
+        return a/b;
 
     };
-    cout << sizeof(f) << endl;
+    cout << sizeof(f) << " : " << f(12,2) << endl;
 
 
     f(x,y);
