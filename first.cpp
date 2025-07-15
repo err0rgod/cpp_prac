9169d0d8c0245cc55d8d0b8d7b59ff2c9c3bff68
err0rgod <ndgaming458@gmail.com>
2025-07-16 00:03:21 +0530

    yo

--- first.cpp
+++ first.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 
-template <typename T> T max(T a,T b);
+template <typename T> T maxi(T a,T b);
 
-template <typename T> T max(T a,T b){
+template <typename T> T maxi(T a,T b){
     return (a>b)? a : b;
    }
 
 int main(){
 
    int a = 10, b = 12;
-   cout << max(a,b) << endl;
+   cout << maxi(a,b) << endl;
    
    
 
